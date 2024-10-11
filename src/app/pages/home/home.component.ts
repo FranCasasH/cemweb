0759bfa07b888670808c4bd8b87f0db12bd19d99
FranCasasH <franciscocasasher@gmail.com>
2024-10-11 13:26:05 -0300

    cursor is broken

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -233,5 +233,35 @@ export class HomeComponent implements AfterViewInit {
     
 
     }
+    if (typeof document !== 'undefined') {
+      const links = document.querySelectorAll('main > .hover-this');
+      const cursor = document.querySelector('.cursor') as HTMLElement;
+
+      const animateit: EventListener = function (this: HTMLElement, e: Event) {
+        const event = e as MouseEvent;
+        const span = this.querySelector('p') as HTMLElement;
+        const { offsetX: x, offsetY: y } = event;
+        const { offsetWidth: width, offsetHeight: height } = this;
+        const move = 25;
+        const xMove = (x / width) * (move * 2) - move;
+        const yMove = (y / height) * (move * 2) - move;
+        span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+        if (event.type === 'mouseleave') span.style.transform = '';
+      };
+
+      const editCursor: EventListener = (e: Event) => {
+        const event = e as MouseEvent;
+        const { clientX: x, clientY: y } = event;
+        cursor.style.left = `${x}px`;
+        cursor.style.top = `${y}px`;
+      };
+
+      links.forEach(link => {
+        link.addEventListener('mousemove', animateit);
+        link.addEventListener('mouseleave', animateit);
+      });
+
+      window.addEventListener('mousemove', editCursor);
+    }
   }
 }
